29e6f472d094760067ed66e58b91720f30f364ff
Martin Thoma <info@martin-thoma.de>
2015-06-18 11:13:37 +0200

    models/baseline-2-c2-2015-05-31-14-00: Fixed training

--- models/baseline-2-c2-2015-05-31-14-00/training.py
+++ models/baseline-2-c2-2015-05-31-14-00/training.py
@@ -19,6 +19,7 @@ os.chdir("../baseline-1-c2-2015-05-31-14-00")
 if not os.path.isfile("model-1.json"):
     os.system("train.py")
 shutil.copyfile("model-1.json", "../baseline-2-c2-2015-05-31-14-00/model-1.json")
+os.chdir("../baseline-2-c2-2015-05-31-14-00")
 
 # Remove last layer
 logging.info("Remove last layer")
@@ -29,7 +30,6 @@ with open("model-1.json", "w") as f:
     json.dump(model, f)
 
 # Make new layer
-os.chdir("../baseline-2-c2-2015-05-31-14-00")
 os.system("detl make mlp 500:500:377 > layer2.json")
 os.system("detl stack model-1.json layer2.json > model-2.json")
 os.system("rm layer2.json")
