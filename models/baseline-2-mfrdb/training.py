0b9f976c2cba3750883d64b78d2bc032b1103617
Martin Thoma <info@martin-thoma.de>
2015-06-27 12:36:21 +0200

    Added MfrDB experiment

---
+++ models/baseline-2-mfrdb/training.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""Train this model with supervised layer-wise pretraining."""
+
+import os
+import shutil
+import json
+
+import logging
+import sys
+
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.DEBUG,
+                    stream=sys.stdout)
+
+# Make model with one hidden layer
+logging.info("Make model with one hidden layer")
+os.chdir("../baseline-1-mfrdb")
+if not os.path.isfile("model-1.json"):
+    os.system("train.py")
+shutil.copyfile("model-1.json", "../baseline-2-mfrdb/model-1.json")
+os.chdir("../baseline-2-mfrdb")
+
+# Remove last layer
+logging.info("Remove last layer")
+with open("model-1.json") as f:
+    model = json.load(f)
+model['layers'].pop()
+with open("model-1.json", "w") as f:
+    json.dump(model, f)
+
+# Make new layer
+os.system("detl make mlp 500:500:94 > layer2.json")
+os.system("detl stack model-1.json layer2.json > model-2.json")
+os.system("rm layer2.json")
+
+logging.info("Train it")
+os.system("train.py")
